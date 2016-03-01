584d8551744e484966f6ed7321c6b910e9f855d1
Jefferson <jefferson_andres@hotmail.com>
2016-03-01 19:49:22 +0100

    Conexión a Base de Datos

--- Simulador/src/controlador/AdminMain.java
+++ Simulador/src/controlador/AdminMain.java
@@ -1,36 +1,35 @@
 package controlador;
 
-import java.beans.PropertyVetoException;
-
+//import java.beans.PropertyVetoException;
+import java.sql.*;
 //import com.mchange.v2.c3p0.ComboPooledDataSource;
-//
 //import bd.Controlador;
 //import gui.AdminWindow;
 
 public class AdminMain {
 
 	public static void main(String[] args) {
-			
-//		try {			
-//			ComboPooledDataSource cpds = new ComboPooledDataSource();
-//			cpds.setDriverClass("org.gjt.mm.mysql.Driver");
-//			cpds.setJdbcUrl("jdbc:mysql://localhost/TFG");
-//			cpds.setUser("AdminTFG");
-//			cpds.setPassword("AdminTFG");
-//			cpds.setAcquireRetryAttempts(1);
-//			cpds.setAcquireRetryDelay(1);
-//			cpds.setBreakAfterAcquireFailure(true);
-//			
-// 			Controlador controla = new Controlador(cpds);
-// 			AdminWindow ventana = new AdminWindow(controla);			
-//			
-//		}
-//		catch (PropertyVetoException e) {
-//			e.printStackTrace();
-//		}
+		
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+	        String url = "jdbc:oracle:thin:@localhost:1521/XE";
+
+		    Connection conn = DriverManager.getConnection(url,"simulador","simulador");
+		    
+		    Controlador controla = new Controlador(conn);
+ 			AdminWindow ventana = new AdminWindow(controla);
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException c){
+			c.printStackTrace();
+		}
+
+	  }
 //
 //	}
 
-	}
+	
 
 }
--- Simulador/src/controlador/Controlador.java
+++ Simulador/src/controlador/Controlador.java
@@ -1,11 +1,16 @@
 package controlador;
 
+import java.sql.Connection;
 import javax.sql.DataSource;
 
 
 public class Controlador {
 
 	public Controlador(DataSource ds){ //acceso a base de datos/servidor web
+		
+	}
+	
+	public Controlador(Connection c){
 		
 	}
 
