b6b1c5327f803e930b04dab95eac268dd03ca7c9
Jefferson <jefferson_andres@hotmail.com>
2016-03-03 21:17:31 +0100

    Actualización de la GUI Principal

--- Simulador/src/controlador/UserMain.java
+++ Simulador/src/controlador/UserMain.java
@@ -17,6 +17,26 @@ public class UserMain extends Application {
 	
 	 private Stage primaryStage;  //escenario, es el primer marco
 	 private BorderPane rootLayout;
+	 
+	 @Override
+	 public void start(Stage primaryStage) throws Exception {
+		 this.primaryStage = primaryStage;
+		 this.primaryStage.setTitle("Simulador");
+
+		 initRootLayout();
+
+		 showLoginView();
+		 
+		 /*Text text = new Text(10, 40, "Hello World!");
+	        text.setFont(new Font(40));
+	        Scene scene = new Scene(new Group(text));
+
+	        stage.setTitle("Welcome to JavaFX!"); 
+	        stage.setScene(scene); 
+	        stage.sizeToScene(); 
+	        stage.show();*/ 
+			
+	}
 	
 	 /**
      * usamos datos que son una lista de Usuarios.
@@ -37,64 +57,55 @@ public class UserMain extends Application {
 		 datosUsuario.add(new Usuario("Martin", "00009"));
 	    }
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 /**
+	  * Initializes the root layout.
+	  */
+	 public void initRootLayout() {
+		 try {
+	         //  Cargas el layout principal  del archivo fxml
+	         FXMLLoader loader = new FXMLLoader();
+	         loader.setLocation(UserMain.class.getResource("vista/RootLayout.fxml"));
+	         rootLayout = (BorderPane) loader.load();
+	            
+	         // Show the scene containing the root layout.
+	         Scene scene = new Scene(rootLayout);
+	         primaryStage.setScene(scene);
+	         primaryStage.show();
+	     }
+		 catch (IOException e) {
+	         e.printStackTrace();
+	     }
+	 }
+	 
 
+	   //si hubiera otro escenario dentro del root layout
+	 public void showLoginView() {
+		 try {
+	         // Load person overview.
+	         FXMLLoader loader = new FXMLLoader();
+	         loader.setLocation(UserMain.class.getResource("vista/LoginUsuario.fxml"));
+	         AnchorPane personOverview = (AnchorPane) loader.load();
+
+	         // Set person overview into the center of root layout.
+	         rootLayout.setCenter(personOverview);
+
+	         // Give the controller access to the main app.
+	         /*PersonOverviewController controller = loader.getController();
+	            controller.setMainApp(this);*/
+
+	     } catch (IOException e) {
+	          e.printStackTrace();
+	     }
+	 }
+	 
+	 	 
+	 public Stage getPrimaryStage() {
+		return primaryStage;
 	}
 
-	
-	
-	/**
-     * Initializes the root layout.
-     */
-    public void initRootLayout() {
-        try {
-            //  Cargas el layout principal  del archivo fxml
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(UserMain.class.getResource("vista/LoginAdmin.fxml"));
-            rootLayout = (BorderPane) loader.load();
-            
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-   //si hubiera otro escenario dentro del root layout
-//    public void showPersonOverview() {
-//        try {
-//            // Load person overview.
-//            FXMLLoader loader = new FXMLLoader();
-//            loader.setLocation(UserMain.class.getResource("vista/PersonOwerview.fxml"));
-//            AnchorPane personOverview = (AnchorPane) loader.load();
-//
-//            // Set person overview into the center of root layout.
-//            rootLayout.setCenter(personOverview);
-//
-//            // Give the controller access to the main app.
-//            PersonOverviewController controller = loader.getController();
-//            controller.setMainApp(this);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    
-	
-	
-	@Override
-	public void start(Stage arg0) throws Exception {
-		
-		        this.primaryStage = primaryStage;
-		        this.primaryStage.setTitle("Login");
-
-		        initRootLayout();
-
-		        //showPersonOverview(); si hubiera otro marco dentro del root layout
-		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.launch(args);
 	}
 
 }
