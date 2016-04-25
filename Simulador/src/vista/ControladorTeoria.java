9ec425b393d681ed57a524a58727713043ba9305
luisredruello <luisredruello89@gmail.com>
2016-04-25 20:07:11 +0200

    Prueba con combobox y pdf

--- Simulador/src/vista/ControladorTeoria.java
+++ Simulador/src/vista/ControladorTeoria.java
@@ -1,11 +1,16 @@
 package vista;
 
+import java.awt.Desktop;
+import java.io.File;
 import java.util.ResourceBundle;
 import javax.print.DocFlavor.URL;
 import controlador.Controlador;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -25,24 +30,97 @@ public class ControladorTeoria implements Initializable, ControlledScreen {
 	
 	private Controlador control;
 	
-//	ObservableList<String> options = FXCollections.observableArrayList(
-//		        "Option 1",
-//		        "Option 2",
-//		        "Option 3"
-//		    );
-	@FXML
-		private ComboBox<String> eligeModulo;
+	String address = " ";
 	
-	//eligeModulo.getItems().addAll("Option4", "Option5", "Option6");
+
+	final ComboBox emailComboBox = new ComboBox();
+	
+	private void recargaCombo () {
+		emailComboBox.getItems().addAll(
+	            "Modulo 1",
+	            "Modulo 2",
+	            "Modulo 3",
+	            "Modulo 4",
+	            "Modulo 5"  
+	        );
+	        emailComboBox.setPromptText("Elige un módulo");
+	        emailComboBox.setEditable(true);        
+	        emailComboBox.valueProperty().addListener(new ChangeListener<String>() {
+	        	@Override 
+	            public void changed(ObservableValue ov, String t, String t1) {                
+	                address = t1;                
+	            }   
+	        });
+	        
+	      
+	     
+	        
+	       
+	}
+	
+	
 	
 	@FXML
 	private void botonTeoriaAction (ActionEvent event){  //AQUÍ HABRÁ QUE ACCEDER A LA BASE DE DATOS
 		
-		//accedo a otra pantalla
-			
+		 botonTeoria.setOnAction(new EventHandler<ActionEvent>() {
+	            @Override
+	            public void handle(ActionEvent e) {
+	                if (emailComboBox.getValue() != null && 
+	                    !emailComboBox.getValue().toString().isEmpty()){
+	                	
+	                	if (emailComboBox.getValue() == ("Modulo 1")){
+	                		cargaPdf ("C:\\Modulo1.pdf");
+	                	}
+	                	else if (emailComboBox.getValue() == ("Modulo 2")){
+	                		cargaPdf ("C:\\Modulo2.pdf");
+	                	}
+	                	else if (emailComboBox.getValue() == ("Modulo 3")){
+	                		cargaPdf ("C:\\Modulo3.pdf");
+	                	}
+	                	else if (emailComboBox.getValue() == ("Modulo 4")){
+	                		cargaPdf ("C:\\Modulo4.pdf");
+	                	}
+	                	else if (emailComboBox.getValue() == ("Modulo 5")){
+	                		cargaPdf ("C:\\Modulo5.pdf");
+	                	}
+	                			
+	            	
+	                }
+	                else {
+	                    
+	                }
+	            }
+	        });
+					
 	}
 	
-	
+private void cargaPdf (String url) {
+    	
+    	try {
+
+    		
+			File pdfFile = new File(url);
+			if (pdfFile.exists()) {
+
+				if (Desktop.isDesktopSupported()) {
+					Desktop.getDesktop().open(pdfFile);
+				} else {
+					System.out.println("Awt Desktop is not supported!");
+				}
+
+			} else {
+				System.out.println("File is not exists!");
+			}
+
+			System.out.println("Done");
+
+		  } catch (Exception ex) {
+			ex.printStackTrace();
+		  }
+
+			
+    }
 	
 	
 	@Override
